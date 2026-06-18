ada03b637c881bd5be2f380846e36e46e75993a6
abdomogahed <abdomogahed2009@gmail.com>
2026-06-18 03:56:37 +0300

    Create selection.cpp

---
+++ selection.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n,n2;
+	char x;
+	cin >> n>>n2;
+	cin >> x;
+	if (x == '*')
+		cout << (n * n2);
+	else if (x == '/')
+		cout << (n / n2);
+	else if (x == '+')
+		cout << (n + n2);
+	else if (x == '-')
+		cout << (n - n2);
+
+
+
+	return 0;
+}
+   
