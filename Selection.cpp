b2402026bcde7f59277e9e93c59557bae72b5303
abdomogahed <abdomogahed2009@gmail.com>
2026-06-18 03:45:43 +0300

    Create Selection.cpp

---
+++ Selection.cpp
@@ -0,0 +1,24 @@
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout << "enter 1 num \n";
+	cin >> n;
+	if (n < 10)
+		cout << "1";
+	else if (n < 100)
+		cout << "2";
+	else if (n < 1000)
+		cout << "3";
+	else if (n < 10000)
+		cout << "4";
+	else if (n < 100000)
+		cout << "5";
+                                                        // بعد ارقام الرقم
+
+	return 0;
+}
+  
